342de59acf4e1ccb69515d3f9c1489c04865193d
bruceli <isanoob1897@outlook.com>
2025-01-05 13:05:20 -0800

    add character creation elements

---
+++ app/src/test/java/dev/desktop/Game.java
@@ -0,0 +1,24 @@
+package dev.desktop;
+import java.util.Scanner;
+
+public class Game {
+    public static double version = 0.1;
+    public static void main(String[] args) {
+        System.out.println("glassBridge:v" + version);
+        String answer = ask("Create a new player? Y, n");
+        if ("Y".equals(answer)) {
+            answer = ask("Enter your player number, 1 - 456");
+            //todo implement checking of number
+        }
+    }
+
+    /**
+     * @param message
+     * @return input
+     */
+ public static String ask(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(message);
+        return in.nextLine();
+    }
+}
---
+++ app/src/test/java/dev/desktop/player.java
@@ -0,0 +1,8 @@
+package dev.desktop;
+
+public class player {
+    public int num;
+    public player(int number) {
+        num = number;
+    }
+}
